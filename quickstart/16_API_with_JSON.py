952a024edc14999d225f834ac863fad2fcde071d
Romulo Pauliv <romulopauliv@bk.ru>
2022-11-25 10:31:50 -0300

    modified the 16_API_with_JSON

--- quickstart/16_API_with_JSON.py
+++ quickstart/16_API_with_JSON.py
@@ -17,3 +17,28 @@ return a dict or list from a view, it will be converted to a JSON response.
 # +--------------------------------------------------------------------------------------------------------------------+
 from flask import Flask
 # +--------------------------------------------------------------------------------------------------------------------+
+
+app = Flask(__name__)
+
+
+@app.route('/me')
+def me_api():
+    return {
+        "username": 'admin',
+        "password": '0000'
+    }
+
+
+@app.route("/users")
+def users_api():
+    users = ['admin', 'root', 'user', 'guest']
+    return [user for user in users]
+
+"""
+This is a shortcut to passing the data to the jsonify() function, which will serialize any supported JSON data type.
+That means that all the data in the dict or list must be JSON serialize.
+
+For complex types such as database models, you'll want to user a serialization library to convert the data to valid JSON
+type first. There are many serialization libraries and Flask API extensions maintained by the community that support
+more complex applications.
+"""
