baee6dd9df85e3485ca4a774fb7abe0112a23753
Romulo Pauliv <romulopauliv@bk.ru>
2022-12-05 10:43:15 -0300

    added the blog functions - post, delete, update

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -20,7 +20,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -55,13 +55,13 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
     ).fetchone()
 
-    if post in None:
+    if post is None:
         abort(404, f"Post id {id} doesn't exist.")
     
     if check_author and post['author_id'] != g.user['id']:
@@ -98,12 +98,11 @@ def update(id):
     return render_template('blog/update.html', post=post)
 
 
-@bp.route('<int:id>/delete', methods=('POST',))
+@bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
     db = get_db()
-    db.execute('DELETE FORM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
-    
